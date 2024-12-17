1163865b8103ac7164a751a5d45e178450a29933
Maksym Balatsko <maksym.balatsko-ext@arista.com>
2024-12-17 14:57:58 +0100

    solve `elon-toys`

--- need-for-speed/need_for_speed.go
+++ need-for-speed/need_for_speed.go
@@ -37,7 +37,7 @@ func Drive(car Car) Car {
 // CanFinish checks if a car is able to finish a certain track.
 func CanFinish(car Car, track Track) bool {
 	movesNeeded := track.distance / car.speed
-	if track.distance%car.speed > 1 {
+	if track.distance%car.speed > 0 {
 		movesNeeded += 1
 	}
 
